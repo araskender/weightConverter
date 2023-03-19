9b6f2dff0339d04197650f12153c56a0925df2c8
Md Ashiqur Rahman <38679297+araskender@users.noreply.github.com>
2023-03-19 16:33:58 +0600

    Add files via upload

---
+++ script/project.js
@@ -0,0 +1,19 @@
+// part1 convert here----------------
+
+function dhaka(num){
+    document.getElementById("outputM").innerHTML = num/1000;
+};
+
+
+// part2 convert here----------------
+
+function khulna(numb){
+    document.getElementById("outputT").innerHTML = numb/107;
+};
+
+
+// part3 convert here----------------
+
+function dim(numbe){
+    document.getElementById("outputD").innerHTML = numbe/12;
+};
